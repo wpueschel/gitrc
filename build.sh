76da7960e58add61ef1913c20601ad1b645a21f3
Willi Püschel <wpueschel@gmail.com>
2018-03-16 13:52:13 +0100

    Updated build script. Windows binary builds first

--- build.sh
+++ build.sh
@@ -2,20 +2,19 @@
 
 cd $(dirname $0)
 
-echo "Building linux binary"
-GOARCH=amd64
-GOOS=linux
-export GOARCH GOOS
-
-go build -o gitrc_linux-amd64
-
-ls -l gitrc_linux-amd64
-
 echo "Building Windows binary"
 GOARCH=386
 GOOS=windows
+export GOARCH GOOS
 
 go build -o gitrc_windows-386.exe
 ls -l gitrc_windows-386.exe
+
+echo "Building linux binary"
+GOARCH=amd64
+GOOS=linux
+
+go build -o gitrc_linux-amd64
+ls -l gitrc_linux-amd64
 
 # EOF
